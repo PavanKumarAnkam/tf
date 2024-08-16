bd1cc85c18f656f6ec4c409dbd8e39e8dbd8f1fa
PavanKumarAnkam <pa1kumarankam@gmail.com>
2024-08-16 12:24:43 +0530

    update

---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.62.0"
+    }
+  }
+}
+
+#provide authentication here
+provider "aws" {
+  region = "us-east-1"
+}
+
+
